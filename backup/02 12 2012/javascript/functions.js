deed631150be6504fa9f6cb3abe7ca5d60b2c623
Robb C <robbchar@gmail.com>
2012-02-12 14:28:11 -0800

    backup files

---
+++ backup/02 12 2012/javascript/functions.js
@@ -0,0 +1,34 @@
+/* Declare a namespace for the site */
+var Site = window.Site || {};
+
+/* Create a closure to maintain scope of the '$'
+   and remain compatible with other frameworks.  */
+(function($) {
+	
+	//same as $(document).ready();
+	$(function() {
+		
+		
+
+	});
+
+
+	$(window).bind("load", function() {
+		
+		
+	
+	});
+	
+})(jQuery);
+
+function loadStaticContent(){
+	// local
+    //$("nav").load("./content/nav.html");
+	$("section.header").load("./content/header.html");
+	$("footer").load("./content/footer.html");
+	
+	// remote
+    //$("nav").load("/content/nav.html");
+	//$("header").load("/content/header.html");
+	//$("footer").load("/content/footer.html");
+ }
